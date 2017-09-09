feeb2335d935fc137d9267f621065eb395c7af6b
Oliver Kopp <kopp.dev@gmail.com>
2017-09-09 10:42:27 +0200

    Remove @formatter=on/off settings

--- io.github.adr.embedded.demo/src/main/java/io/github/adr/embedded/demo/ADAnnotationDemoClass.java
+++ io.github.adr.embedded.demo/src/main/java/io/github/adr/embedded/demo/ADAnnotationDemoClass.java
@@ -9,14 +9,12 @@ import io.github.adr.embedded.DecisionMade;
 @ArchitecturallySignificant(concerns = "performance and scalability needs", nfrs = {
         ArchitecturallySignificant.QualityAttribute.PERFORMANCE,
         ArchitecturallySignificant.QualityAttribute.MANAGEABILITY})
-// @formatter:off
 @DecisionMade(id = "AD-001",
         solvedProblem = "Need to select a single consistent ComponentImplementationParadigm' for entire business logic layer",
         chosenOption = "Decided to implement business logic as a 'POJO' (and not as a framework component such as 'JEE EJB' or 'SpringBean')",
         // neglectedAlternatives={"JEE EJB", "Spring bean"},
         rationale = "because it is a company-wide policy, see http://www.iaas.uni-stuttgart.de/institut/ehemalige/zimmermann/indexE.php",
         relatedDecisions = {"AD-002", "AD-003"})
-//@formatter:on
 public class ADAnnotationDemoClass {
 
     @DecisionMade(id = "AD-002", solvedProblem = "tbd", chosenOption = "tbd")
--- io.github.adr.embedded.demo/src/main/java/io/github/adr/embedded/demo/YStatementAnnotationDemoClass.java
+++ io.github.adr.embedded.demo/src/main/java/io/github/adr/embedded/demo/YStatementAnnotationDemoClass.java
@@ -3,11 +3,9 @@ package io.github.adr.embedded.demo;
 
 import io.github.adr.embedded.YStatementJustification;
 
-//@formatter:off
 @YStatementJustification(
         chosen = "we decided to implement business logic in POJOs",
         neglected = "other options such as JEE EJBs and Spring components")
-//@formatter:on
 public class YStatementAnnotationDemoClass {
 
     public int sampleBusinessLogic(String input) {
