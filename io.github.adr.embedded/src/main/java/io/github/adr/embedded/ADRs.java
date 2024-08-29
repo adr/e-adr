280425b96dc833e0f48abcbb6352940a8010ffb0
Oliver Kopp <kopp.dev@gmail.com>
2024-08-29 08:38:29 +0200

    Add info why *s is required

--- io.github.adr.embedded/src/main/java/io/github/adr/embedded/ADRs.java
+++ io.github.adr.embedded/src/main/java/io/github/adr/embedded/ADRs.java
@@ -4,6 +4,9 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Required for <code>@Repeatable</code> at {@link ADR}.
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Documented
 public @interface ADRs {
--- io.github.adr.embedded/src/main/java/io/github/adr/embedded/MADRs.java
+++ io.github.adr.embedded/src/main/java/io/github/adr/embedded/MADRs.java
@@ -6,6 +6,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Required for <code>@Repeatable</code>.
+ */
 @Target({
     ElementType.ANNOTATION_TYPE,
     ElementType.CONSTRUCTOR,
